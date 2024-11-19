efcabeaa5dbe94724dc9eb803ea164f282fda012
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-11-19 22:35:58 +0100

    Add pip upgrade flag

--- python/upload.sh
+++ python/upload.sh
@@ -3,6 +3,6 @@
 # remember to update the README.rst file
 #pandoc --from=markdown --to=rst --output README.rst README.md
 #pandoc --from=markdown --to=rst --output CHANGELOG.rst CHANGELOG.md
-venv/bin/python -m pip install build
+venv/bin/python -m pip install --upgrade build
 venv/bin/python -m build
 venv/bin/twine upload dist/* --verbose
